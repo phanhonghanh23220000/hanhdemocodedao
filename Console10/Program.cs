0d4e060ac8d96b544fb548bcff862583d367f063
Hanh Coder <133193344+phanhonghanh23220000@users.noreply.github.com>
2023-06-11 00:22:53 +0700

    update

---
+++ Console10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A DAY ROI");
+        }
+    }//LUU O DAU nhấn tổ hợp phím C
+}
