365ac71f2ff9c1e531b79080626f80928a071fe2
Hanh Coder <133193344+phanhonghanh23220000@users.noreply.github.com>
2023-06-11 00:15:55 +0700

    update code

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ví dụ đây là một bài code mới a chỉ cần làm xong rồi ctrl + s ở bên visua studio xong nhấn sang git desktop");
